5fbdcd4d7b779a1baa7e63ab3f77bbe0ebd2076c
YutoMasamura <yutomasamura@YutoMasamuranoMacBook-puro.local>
2018-11-17 23:21:38 +0900

    delete_and_edit_pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,32 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    respond_to do |format|
+      @user.update(user_params)
+        format.html { redirect_to @user }
+        format.json { render :show }
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:name, :email, :tel)
+    end
 end
