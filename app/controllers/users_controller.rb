6c2df44f1decd42f3cd5b328908e844ba63746d0
YutoMasamura <yutomasamura@YutoMasamuranoMacBook-puro.local>
2018-11-25 21:42:16 +0900

    deleteアクションのエラー修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,7 +38,7 @@ class UsersController < ApplicationController
   def destroy
     @user.destroy
     respond_to do |format|
-      format.html { redirect_to users_url }
+      format.html { redirect_to root_url }
     end
   end
 
