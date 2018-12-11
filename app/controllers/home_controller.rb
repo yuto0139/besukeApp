e93716488a984b491464b664e27c98e4d4016a75
YutoMasamura <yutomasamura@YutoMasamuranoMacBook-puro.local>
2018-12-11 14:27:33 +0900

    tab_menu

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 class HomeController < ApplicationController
   before_action :authenticate_customer!, only: :authentication
-  def index; end
-
+  def index; end 
   def authentication
     redirect_to "/users"
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :customers
   root 'home#authentication'
   resources :users, only: %i[index show new edit create update destroy]
+  get '/home/index', to: 'home#index'
 end
