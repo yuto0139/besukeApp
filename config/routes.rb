0eb353c9374231f4835defcd7f1cad81bcdd22c5
YutoMasamura <yutomasamura@YutoMasamuranoMacBook-puro.local>
2018-12-13 19:05:14 +0900

    blogsコントローラーとビューを追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   root 'home#authentication'
   resources :users, only: %i[index show new edit create update destroy]
   get '/home/index', to: 'home#index'
+  resources :blogs do
+    resources :entries, except: :index
+  end
 end
