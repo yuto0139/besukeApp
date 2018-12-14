8ba04db9801cef0560293907c1aa0e219090799b
YutoMasamura <yutomasamura@YutoMasamuranoMacBook-puro.local>
2018-12-14 17:31:44 +0900

    Modelのスペック

--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe Blog, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Blog do
+  it "titleがあれば有効な状態であること" do
+    blog = Blog.new(title: 'Hey!')
+    expect(blog.valid?).to eq true
+  end
+  it "titleがなければ無効な状態であること" do
+    blog = Blog.new(title: '')
+    expect(blog.valid?).to eq false
+  end
 end
