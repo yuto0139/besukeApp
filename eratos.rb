1084f17111e910fe2ed174b3d6c36b683f840a9e
YutoMasamura <yuto.masumura@favy.co.jp>
2019-02-25 10:35:13 +0900

    エラトステネスの篩

---
+++ eratos.rb
@@ -0,0 +1,30 @@
+class Eratos
+
+  def extractPrimes(n)
+    # 2~t の配列を用意する
+    numbers = (2..n).to_a
+    # 素数を入れる配列を用意する
+    primes = []
+    # 1. 先頭の数字を取り出す (先頭の数字がなくなるまで繰り返し)
+    while true
+      top = numbers.shift
+      break if !top
+    # 2. 取り出した先頭の数字を素数の配列に追加
+      primes.push(top)
+    # 3. 取り出した先頭の数字の倍数があれば、配列から削除 (倍数がなくなるまで繰り返し)
+      numbers.each do |x|
+        numbers.delete(x) if x % top == 0
+      end
+    end
+    # 4. 素数の配列を出力する
+    return primes
+  end
+
+  def output (primes)
+    puts primes
+  end
+end
+
+eratos = Eratos.new
+primes = eratos.extractPrimes(ARGV[0].to_i)
+eratos.output(primes)
